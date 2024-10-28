0672f36638a9e8ce55e66816a62a538bd747f3ff
pjsier <pjsier@gmail.com>
2024-10-28 19:25:07 -0400

    refactor: disable logger

--- client/src/middleware/logger.js
+++ client/src/middleware/logger.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line
 const logger = (store) => (next) => (action) => {
-  console.group(action.type)
-  console.log("The action: ", action)
+  // console.group(action.type)
+  // console.log("The action: ", action)
   //this is like dispatching an action
   //to get the next state
   const returnValue = next(action)
-  console.log("The new state: ", store.getState())
-  console.groupEnd()
+  // console.log("The new state: ", store.getState())
+  // console.groupEnd()
 
   return returnValue
 }
