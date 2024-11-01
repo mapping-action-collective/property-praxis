4821208832a127f6207a3d35adbc4972d5e53a55
pjsier <pjsier@gmail.com>
2024-11-01 07:49:18 -0400

    refactor: update time graph style

--- client/src/components/Search/TimeGraph.jsx
+++ client/src/components/Search/TimeGraph.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react"
 import { APISearchQueryFromRoute } from "../../utils/api"
 import {
+  VictoryAxis,
   VictoryChart,
   VictoryLine,
+  VictoryScatter,
   VictoryTheme,
   VictoryTooltip,
 } from "victory"
+import { DEFAULT_YEAR, MINIMUM_YEAR } from "../../utils/constants"
 
 function TimeGraph({ ownid }) {
   const [data, setData] = useState(null)
@@ -13,11 +16,11 @@ function TimeGraph({ ownid }) {
   useEffect(() => {
     ;(async () => {
       if (ownid) {
-        const route = `/api/detailed-search?type=speculator-by-year&ownid=${ownid.toUpperCase()}`
+        const route = `/api/detailed-search?type=speculator-by-year&ownid=${ownid}`
         const data = await APISearchQueryFromRoute(route)
         const graph_data = data
           .map(({ year, count }) => ({
-            x: new Date(year, 1, 1),
+            x: +year,
             y: Number(count),
           }))
           .sort((a, b) => a.year - b.year)
@@ -27,19 +30,32 @@ function TimeGraph({ ownid }) {
     return () => null
   }, [ownid])
 
-  // TODO: Tooltip only on mouseover
   return (
     <VictoryChart
       theme={VictoryTheme.material}
-      labelComponent={<VictoryTooltip />}
-      labels={({ datum, active }) => (active ? datum.label : null)}
+      domainPadding={5}
+      padding={{ top: 25, bottom: 50, left: 40, right: 10 }}
+      domain={{
+        x: [+MINIMUM_YEAR, +DEFAULT_YEAR],
+        y: [0, Math.max(...(data || [{ y: 10 }]).map(({ y }) => y))],
+      }}
     >
+      <VictoryAxis tickFormat={(tick) => `${tick}`} />
+      <VictoryAxis tickFormat={(tick) => `${tick}`} dependentAxis />
       <VictoryLine
         style={{
-          data: { stroke: "#e4002c" },
+          data: { stroke: "#e4002c", strokeWidth: 3 },
           parent: { border: "1px solid #ccc" },
         }}
         data={data}
+      />
+      <VictoryScatter
+        style={{
+          data: { fill: "#e4002c", strokeWidth: 35 },
+        }}
+        size={4}
+        data={(data || []).map((datum) => ({ ...datum, label: datum.y }))}
+        labelComponent={<VictoryTooltip />}
       />
     </VictoryChart>
   )
